ccaa3b81d98b39f86b726b3531f94fa00c5077bf
Francisco Jose Fernandez Garcia <fjfernandez@solucionaris.com>
2017-09-04 20:52:09 +0200

    Creatión of the solution and structure.

---
+++ ClcWorld.Entities/DbContext/IClcWorldContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace ClcWorld.Entities.DbContext
+{
+    interface IClcWorldContext:IDisposable
+    {
+        Database Database { get; }
+        DbChangeTracker GetChangeTracker { get; }
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+        DbEntityEntry Entry(object entity);
+        T EntryWithState<T>(T entity, EntityState state) where T : class;
+        List<string> GetModifiedProperties<T>(T entity) where T : class;
+        DbContextConfiguration GetConfiguration();
+        
+    }
+}
---
+++ ClcWorld.Entities/Entities/CarBrand.cs
@@ -0,0 +1,8 @@
+﻿namespace ClcWorld.Entities.Entities
+{
+    public class CarBrand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
